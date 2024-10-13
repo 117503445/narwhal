6f690e933c65b086cfd67e783a37ced7455fa46e
117503445 <t117503445@gmail.com>
2024-10-13 18:48:37 +0000

    wip
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- qproto/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // println!("cargo:rerun-if-changed=protos/narwhal.proto");
-    println!("qproto");
-    tonic_build::compile_protos("../types/narwhal.proto")?;
-    Ok(())
-}
--- types/src/proto.rs
+++ types/src/proto.rs
@@ -38,7 +38,7 @@ pub use narwhal::{
     NewEpochRequest, NewNetworkInfoRequest, NodeReadCausalRequest, NodeReadCausalResponse,
     PrimaryAddresses as PrimaryAddressesProto, PublicKey as PublicKeyProto, ReadCausalRequest,
     ReadCausalResponse, RemoveCollectionsRequest, RoundsRequest, RoundsResponse,
-    Transaction as TransactionProto, ValidatorData,
+    Transaction as TransactionProto, ValidatorData, ExecuteInfo
 };
 
 impl From<PublicKey> for PublicKeyProto {
