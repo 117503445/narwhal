511e998a83e065f5dd9254f459c84652a3e87e01
117503445 <t117503445@gmail.com>
2024-10-13 23:30:13 +0800

    wip
    Signed-off-by: 117503445 <t117503445@gmail.com>

---
+++ q/dev/command/command.go
@@ -0,0 +1,58 @@
+package command
+
+import (
+	"github.com/117503445/goutils"
+	"github.com/rs/zerolog/log"
+)
+
+type BuildCmd struct {
+}
+
+func (b *BuildCmd) Run() error {
+	deleteOld := false // 删除旧的数据
+	if deleteOld {
+		goutils.Exec("docker compose down", goutils.WithCwd("../Docker"))
+	}
+
+	goutils.Exec("docker compose up -d", goutils.WithCwd("../"))
+
+	// -T 避免 the input device is not a TTY
+	goutils.Exec("docker compose exec -T builder cargo build --target-dir docker-target --bin node --bin benchmark_client", goutils.WithCwd("../"))
+
+	goutils.Exec("docker build -t 117503445/narwhal .", goutils.WithCwd("../"))
+
+	goutils.Exec("docker compose up -d --build", goutils.WithCwd("../Docker"))
+
+	// 等待服务启动
+	// sleepDuration := 3 * time.Second
+	// log.Info().Dur("sleepDuration", sleepDuration).Msg("sleep")
+	// time.Sleep(sleepDuration)
+
+	return nil
+}
+
+type ReqCMD struct {
+}
+
+func (r *ReqCMD) Run() error {
+	goutils.Exec("docker compose exec -T worker_0 ./bin/benchmark_client --nodes http://localhost:4001 --rate 20 --size 10 http://localhost:4001", goutils.WithCwd("../Docker"))
+
+	return nil
+}
+
+type Dev0CMD struct {
+}
+
+func (r *Dev0CMD) Run() error {
+	log.Debug().Msg("dev-0")
+
+	goutils.Exec("go build .", goutils.WithCwd("./assets/fc-worker"), goutils.WithEnv(map[string]string{
+		"GOOS":        "linux",
+		"GOARCH":      "amd64",
+		"CGO_ENABLED": "0",
+	}))
+
+	goutils.Exec("docker compose exec -T --workdir /workspace/q/assets/fc-worker fc s deploy -y", goutils.WithCwd("../"))
+
+	return nil
+}
--- q/main.go
+++ q/main.go
@@ -3,77 +3,21 @@ package main
 import (
 	"github.com/117503445/goutils"
 	"github.com/alecthomas/kong"
-	"github.com/rs/zerolog/log"
-	// "github.com/rs/zerolog/log"
+	dev "q/dev/command"
 )
-
-type Context struct {
-}
 
 type DefaultCmd struct {
 }
 
-func (d *DefaultCmd) Run(ctx *Context) error {
-	return new(BuildCmd).Run(ctx)
-}
-
-type BuildCmd struct {
-}
-
-func (b *BuildCmd) Run(ctx *Context) error {
-	deleteOld := false // 删除旧的数据
-	if deleteOld {
-		goutils.Exec("docker compose down", goutils.WithCwd("../Docker"))
-	}
-
-	goutils.Exec("docker compose up -d", goutils.WithCwd("../"))
-
-	// -T 避免 the input device is not a TTY
-	goutils.Exec("docker compose exec -T builder cargo build --target-dir docker-target --bin node --bin benchmark_client", goutils.WithCwd("../"))
-
-	goutils.Exec("docker build -t 117503445/narwhal .", goutils.WithCwd("../"))
-
-	goutils.Exec("docker compose up -d --build", goutils.WithCwd("../Docker"))
-
-	// 等待服务启动
-	// sleepDuration := 3 * time.Second
-	// log.Info().Dur("sleepDuration", sleepDuration).Msg("sleep")
-	// time.Sleep(sleepDuration)
-
-	return nil
-}
-
-type ReqCMD struct {
-}
-
-func (r *ReqCMD) Run(ctx *Context) error {
-	goutils.Exec("docker compose exec -T worker_0 ./bin/benchmark_client --nodes http://localhost:4001 --rate 20 --size 10 http://localhost:4001", goutils.WithCwd("../Docker"))
-
-	return nil
-}
-
-type Dev0CMD struct {
-}
-
-func (r *Dev0CMD) Run(ctx *Context) error {
-	log.Debug().Msg("dev-0")
-
-	goutils.Exec("go build .", goutils.WithCwd("./assets/fc-worker"), goutils.WithEnv(map[string]string{
-		"GOOS":        "linux",
-		"GOARCH":      "amd64",
-		"CGO_ENABLED": "0",
-	}))
-
-	goutils.Exec("docker compose exec -T --workdir /workspace/q/assets/fc-worker fc s deploy -y", goutils.WithCwd("../"))
-
-	return nil
+func (d *DefaultCmd) Run() error {
+	return new(dev.BuildCmd).Run()
 }
 
 var cli struct {
-	DefaultCmd DefaultCmd `cmd:"" hidden:"" default:"1"`
-	Build      BuildCmd   `cmd:""`
-	Req        ReqCMD     `cmd:""`
-	Dev0       Dev0CMD    `cmd:""`
+	DefaultCmd DefaultCmd   `cmd:"" hidden:"" default:"1"`
+	Build      dev.BuildCmd `cmd:""`
+	Req        dev.ReqCMD   `cmd:""`
+	Dev0       dev.Dev0CMD  `cmd:""`
 }
 
 func main() {
@@ -81,6 +25,6 @@ func main() {
 
 	ctx := kong.Parse(&cli)
 
-	err := ctx.Run(&Context{})
+	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
 }
