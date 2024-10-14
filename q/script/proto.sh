55d099832959632e63af9d1b68f72e4188c0f15b
117503445 <t117503445@gmail.com>
2024-10-15 00:00:49 +0800

    wip
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- q/script/proto.sh
+++ q/script/proto.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /workspace/q
-protoc --go_out=/workspace/q --go-grpc_out=/workspace/q --proto_path /workspace/q /workspace/q/assets/common.proto
+protoc --go_out=/workspace/q --go-grpc_out=/workspace/q --proto_path /workspace/types/proto /workspace/types/proto/narwhal.proto
