f7c0bc49d212c6a2fc3e23ef46ef83136d21853a
117503445 <t117503445@gmail.com>
2024-10-15 18:41:52 +0000

    send-req
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- q/sendreq/command/command.go
+++ q/sendreq/command/command.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"q/rpc"
 
+	"bytes"
+	"encoding/binary"
+
 	"github.com/117503445/goutils"
 	"github.com/rs/zerolog/log"
 
@@ -25,7 +28,27 @@ func (*SendReqCmd) Run() error {
 	}
 	client := rpc.NewTransactionsClient(conn)
 
-	_, err = client.SubmitTransaction(context.Background(), &rpc.Transaction{})
+	size := 1024 // 假设大小为1024
+	var r uint64 = 0
+
+	// 创建一个字节缓冲区
+	tx := make([]byte, size)
+	buf := bytes.NewBuffer(tx[:0])
+
+	// 将标准交易的标识符和计数器值放入缓冲区
+	r++
+	buf.WriteByte(1)                          // 标准交易以 1 开头
+	binary.Write(buf, binary.LittleEndian, r) // 确保所有客户端发送不同的交易
+
+	// 调整缓冲区大小以匹配指定的大小
+	if buf.Len() < size {
+		buf.Write(make([]byte, size-buf.Len()))
+	}
+
+	// 将缓冲区转换为字节数组
+	tx = buf.Bytes()
+
+	_, err = client.SubmitTransaction(context.Background(), &rpc.Transaction{Transaction: tx})
 	if err != nil {
 		log.Fatal().Err(err).Msg("SubmitTransaction")
 	}
