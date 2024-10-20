2aec4ea6c04ed7446c1b7c9e5395a345e46123ef
117503445 <t117503445@gmail.com>
2024-10-20 16:51:47 +0000

    Refactor fc.go to remove unused import and update ListInstancesWithOptions to ListInstances
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- q/worker-master/server/fc.go
+++ q/worker-master/server/fc.go
@@ -5,7 +5,6 @@ import (
 	"q/common"
 
 	fc20230330 "github.com/alibabacloud-go/fc-20230330/v4/client"
-	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
 	"github.com/rs/zerolog/log"
 )
@@ -22,7 +21,7 @@ func NewFcManager(masterID int) *FcManager {
 // GetInfo 获取第 index 个函数计算实例的信息
 func (m *FcManager) GetInfo(index int) string {
 	functionName := tea.String(fmt.Sprintf("biye-%d-%d", m.masterID, index))
-	result, err := common.FcClient.ListInstancesWithOptions(functionName, &fc20230330.ListInstancesRequest{}, make(map[string]*string), &util.RuntimeOptions{})
+	result, err := common.FcClient.ListInstances(functionName, &fc20230330.ListInstancesRequest{})
 	if err != nil {
 		log.Fatal().Err(err).Msg("list instances failed")
 	}
