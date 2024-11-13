01291f5257c4042273cd29733cf9576b065503f9
117503445 <t117503445@gmail.com>
2024-11-14 04:03:42 +0800

    case0
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- q/assets/fc-worker/oss.go
+++ q/assets/fc-worker/oss.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"sync"
+
+	"q/qrpc"
 
 	"github.com/rs/zerolog/log"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"q/qrpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (s *Server) OssCase0Start(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
@@ -21,19 +24,45 @@ func (s *Server) OssCase0Start(context.Context, *emptypb.Empty) (*emptypb.Empty,
 			client = clients[0]
 		}
 
-		_, err = client.OssCase0SendBatch(context.Background(), &qrpc.OssCase0Batch{})
-		if err != nil {
-			log.Error().Err(err).Msg("OssCase0SendBatch")
-		}
-		log.Info().Msg("Send OssCase0SendBatch")
+		// 10MB
+		payload := make([]byte, 10*1024*1024)
+		for {
+			_, err = client.OssCase0SendBatch(context.Background(), &qrpc.OssCase0Batch{Payload: payload})
 
+			if err != nil {
+				log.Error().Err(err).Msg("OssCase0SendBatch")
+			}
+			log.Info().Msg("Send OssCase0SendBatch")
+		}
 	}()
 
 	return &emptypb.Empty{}, nil
 }
 
-func (s *Server) OssCase0SendBatch(context.Context, *qrpc.OssCase0Batch) (*emptypb.Empty, error) {
+func (s *Server) OssCase0SendBatch(ctx context.Context, batch *qrpc.OssCase0Batch) (*emptypb.Empty, error) {
 	log.Info().Msg("Recv OssCase0SendBatch")
+	metricsLock.Lock()
+	defer metricsLock.Unlock()
+
+	metrics.Batches = append(metrics.Batches, &qrpc.OssCase0BatchMeta{
+		ReceivedAt: timestamppb.Now(),
+		Size:       int64(len(batch.Payload)),
+	})
+
+	log.Info().Int("len", len(metrics.Batches)).Msg("OssCase0SendBatch")
 
 	return &emptypb.Empty{}, nil
+}
+
+var metrics = &qrpc.OssCase0Metrics{}
+var metricsLock sync.Mutex
+
+func (s *Server) OssCase0GetMetrics(context.Context, *emptypb.Empty) (*qrpc.OssCase0Metrics, error) {
+	log.Info().Msg("OssCase0GetMetrics")
+	metricsLock.Lock()
+	defer metricsLock.Unlock()
+
+	log.Info().Int("len", len(metrics.Batches)).Msg("OssCase0GetMetrics")
+
+	return metrics, nil
 }
--- q/dev/command/oss.go
+++ q/dev/command/oss.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/117503445/goutils"
 	"github.com/rs/zerolog/log"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"q/qrpc"
@@ -28,14 +30,47 @@ func (c *Oss0CaseCMD) Run() error {
 	w := DeployECI(2, 1)
 	log.Info().Interface("w", w).Msg("DeployECI")
 
-	internetIp := w.Workers[0].InternetIp
+	c1InternetIp := w.Workers[0].InternetIp
+	c2InternetIp := w.Workers[1].InternetIp
 
-	c1 := qrpc.NewWorkerSlaveProtobufClient(fmt.Sprintf("http://%s:9000", internetIp), &http.Client{})
+	time.Sleep(3 * time.Second)
+
+	c1 := qrpc.NewWorkerSlaveProtobufClient(fmt.Sprintf("http://%s:9000", c1InternetIp), &http.Client{})
 	_, err = c1.OssCase0Start(context.Background(), &emptypb.Empty{})
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to call OssCase0Start")
 	}
-	log.Info().Str("internetIp", internetIp).Msg("OssCase0Start")
+	log.Info().Str("internetIp", c1InternetIp).Msg("OssCase0Start")
+
+	c2 := qrpc.NewWorkerSlaveProtobufClient(fmt.Sprintf("http://%s:9000", c2InternetIp), &http.Client{})
+	for {
+		metrics, err := c2.OssCase0GetMetrics(context.Background(), &emptypb.Empty{})
+		if err != nil {
+			log.Error().Err(err).Msg("failed to call OssCase0GetMetrics")
+			time.Sleep(time.Second * 3)
+			continue
+		}
+		log.Info().Str("metrics",
+			protojson.Format(metrics)).Msg("OssCase0GetMetrics")
+
+		if len(metrics.Batches) <= 1 {
+			time.Sleep(time.Second * 3)
+			continue
+		}
+
+		startTime := metrics.Batches[0].ReceivedAt
+		endTime := metrics.Batches[len(metrics.Batches)-1].ReceivedAt
+		durSeconds := endTime.Seconds - startTime.Seconds
+		sizeSum := 0
+		for _, batch := range metrics.Batches {
+			sizeSum += int(batch.Size)
+		}
+		bps := sizeSum / int(durSeconds)
+		log.Info().Int64("dur", int64(durSeconds)).Int("sizeSum", sizeSum).Int("bps(MB)", bps/1024/1024).Msg("OssCase0GetMetrics")
+
+		time.Sleep(time.Second * 10)
+
+	}
 
 	return nil
 }
