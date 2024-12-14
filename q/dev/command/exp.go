e5a014269417c7bd5a023a90895716e223c8390b
117503445 <t117503445@gmail.com>
2024-12-14 14:00:49 +0000

    wip
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- q/dev/command/exp.go
+++ q/dev/command/exp.go
@@ -53,7 +53,8 @@ func runExp2() {
 func runExp3() {
 	pressList := []int{100000}
 	// pressList := []int{1000,100000}
-	nList := []int{4, 8, 16, 32, 64}
+	// nList := []int{4, 8, 16, 32, 64}
+	nList := []int{64}
 
 	for _, press := range pressList {
 		for _, n := range nList {
@@ -67,6 +68,6 @@ func runExp3() {
 }
 
 func (cmd *ExpCMD) Run() error {
-	runExp2()
+	runExp3()
 	return nil
 }
